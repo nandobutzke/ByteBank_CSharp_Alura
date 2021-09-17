9739129da625ccf01ac443966d699a92a88a6963
Fernando Butzke <nandoproject209@gmail.com>
2021-09-17 20:49:52 -0300

    Adjusting file paths

---
+++ Exceções/SaldoInsuficienteException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_Alura_CSharp.Exceções
+{
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public double ValorSaque { get; }
+        public double Saldo { get; }
+
+        public SaldoInsuficienteException() { }
+
+        public SaldoInsuficienteException(double valorSaque, double saldo)
+            : this("O saque no valor de " + valorSaque + " não pode ser efetuado " +
+                  "pois o saldo é de " + saldo)
+        {
+            ValorSaque = valorSaque;
+            Saldo = saldo;
+        }
+
+        public SaldoInsuficienteException(string mensagem) : base(mensagem) { }
+
+        public SaldoInsuficienteException(string mensagem, Exception exceptionInterna) 
+            : base(mensagem, exceptionInterna)
+        {
+
+        }
+    }
+}
